2cb91afc0e1437f9c273a74b2cedfef63f05d107
GaJin Kim <gajin2000@gmail.com>
2021-12-12 18:55:14 -0600

    Finished Day 9

--- src/Day9.java
+++ src/Day9.java
@@ -2,6 +2,35 @@ package src;
 
 public class Day9 {
     public static void main(String[] args) {
-        System.out.println("hi");
+        int[] numbers = {35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576};
+        int[] preamble = new int[5];
+
+        for (int i = 0; i < numbers.length - preamble.length; i++) {
+            // update the preamble
+            for (int j = 0; j < preamble.length; j++)
+                preamble[j] = numbers[j + i];
+
+            boolean foundSum = false;
+            int validNum = numbers[i + preamble.length];
+
+            // find a combination of 2 numbers that will sum up to validNum
+            for (int k = 0; k < preamble.length - 1; k++) {
+                int numOne = preamble[k];
+                for (int l = k + 1; l < preamble.length; l++) {
+                    int numTwo = preamble[l];
+                    if (numOne + numTwo == validNum) {
+                        System.out.println(validNum + " = " + numOne + " + " + numTwo);
+                        foundSum = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!foundSum) {
+                System.out.println("Could not find a summation for " + validNum);
+                break;
+            }
+        }
+
     }
 }
