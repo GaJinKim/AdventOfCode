b57a383309093f5b934494dcdf09201ba63f66d0
GaJin Kim <gajin2000@gmail.com>
2021-12-12 19:02:49 -0600

    Finished Day 9

--- src/Day9.java
+++ src/Day9.java
@@ -1,5 +1,8 @@
 package src;
 
+/**
+ * https://adventofcode.com/2020/day/9
+ */
 public class Day9 {
     public static void main(String[] args) {
         int[] numbers = {35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576};
